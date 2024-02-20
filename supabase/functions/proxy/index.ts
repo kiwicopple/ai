98e1868145340e33ad363eae3e052c0496253334
Copple <10214025+kiwicopple@users.noreply.github.com>
2024-02-20 21:20:38 +0100

    removes JWT from edge function

--- supabase/functions/proxy/index.ts
+++ supabase/functions/proxy/index.ts
@@ -8,7 +8,6 @@
   2. Make an HTTP request:
 
   curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/proxy' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
     --header 'Content-Type: application/json' \
     --data '{
         "model": "llama2",
